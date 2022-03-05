e2d94479ea2009b4ed5e1d3bc0747111894b1a4b
NailyaBash <100082134+NailyaBash@users.noreply.github.com>
2022-03-05 14:16:18 +0500

    Add files via upload
    Виджеты - переключатели  CheckBox, Switch, Radio. Виджеты с состоянием - StatefullWidget

---
+++ lib/switches_demo_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class SwitchesDemoScreen extends StatefulWidget {
+  const SwitchesDemoScreen({Key? key}) : super(key: key);
+
+  @override
+  _SwitchesDemoScreenState createState() => _SwitchesDemoScreenState();
+}
+ enum SkillLevel {junior, middle, senior }
+class _SwitchesDemoScreenState extends State<SwitchesDemoScreen> {
+  bool _cheked = false;
+  bool _confirmAgreement = true;
+  SkillLevel? _skillLevel=SkillLevel.junior;
+
+void _onCheckedChange(bool? val){
+  setState(() {
+    _cheked = !_cheked;
+  });
+}
+  void _onSkilllevelChanged (SkillLevel? value) {
+  setState(() {
+  _skillLevel = value;
+  });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Center(
+          child: Column(children: [
+            Row(
+              children: [
+                Checkbox(value: _cheked, onChanged: _onCheckedChange),
+                Text ('Выбор'),
+              ],
+            ),
+            CheckboxListTile(
+                title: Text ('Принять условия соглашения'),
+                value: _confirmAgreement, onChanged: (val){
+                  setState(() {
+                    _confirmAgreement=!_confirmAgreement;
+                  });
+            }),
+            Row(
+              children: [
+                Switch(value: _cheked, onChanged: _onCheckedChange),
+                const Text('Включить')
+              ],
+            ),
+            SwitchListTile(
+              title: Text('Включить'),
+                value: _cheked,
+                onChanged: _onCheckedChange),
+            const Text ('Уровень навыков'),
+            RadioListTile<SkillLevel>(
+              title: const Text('junior'),
+              value: SkillLevel.junior,
+              groupValue: _skillLevel,
+              onChanged: _onSkilllevelChanged,
+            ),
+            RadioListTile<SkillLevel>(
+              title: const Text('middle'),
+              value: SkillLevel.middle,
+              groupValue: _skillLevel,
+              onChanged: _onSkilllevelChanged,
+            ),
+            RadioListTile<SkillLevel>(
+              title: const Text('senior'),
+              value: SkillLevel.senior,
+              groupValue: _skillLevel,
+              onChanged: _onSkilllevelChanged,
+            ),
+  ]))));
+  }
+}
